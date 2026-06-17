af36ad63d7494fdfaeb75eb989e2936dd001b902
feraistanti <fistanti551@gmail.com>
2026-06-18 01:17:10 +0700

    Repository untuk mengelola data profil pada database

---
+++ src/main/java/com/example/pertemuan11/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.example.pertemuan11.repository;
+
+import com.example.pertemuan11.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, String> {
+}
